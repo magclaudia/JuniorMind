f578702c23c456e46e65393c9571bed4d469c906
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-01-31 14:06:25 +0200

    add Binary Search

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = ReadNumbers();
+            int numberToFind = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(BinarySearch(numbers, numberToFind));
+            Console.Read();
+        }
+
+        private static int[] ReadNumbers()
+        {
+            string[] numbers = Console.ReadLine().Split();
+            int[] result = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = Convert.ToInt32(numbers[i]);
+            }
+
+            return result;
+        }
+
+        static int BinarySearch(int[] numbers, int numberToFind)
+        {
+            throw new NotImplementedException();
+        }
+
+        static bool CheckNumberAtIndex(int[] numbers, int index, int numberToCheck)
+        {
+            Console.WriteLine("Checking index " + index + " (value " + numbers[index] + ")");
+            return numbers[index] == numberToCheck;
+        }
+    }
+}
