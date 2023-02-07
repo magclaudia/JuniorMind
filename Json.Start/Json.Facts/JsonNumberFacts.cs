02dd821321066f54ca3a096966e5a88cd4ae7914
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-07 15:02:05 +0200

    Test IsFraction

--- Json.Start/Json.Facts/JsonNumberFacts.cs
+++ Json.Start/Json.Facts/JsonNumberFacts.cs
@@ -59,5 +59,36 @@ namespace Json.Facts
         {
             Assert.True(IsJsonNumber("-0"));
         }
+
+        [Fact]
+        public void CanBeFractional()
+        {
+            Assert.True(IsJsonNumber("12.3e4"));
+        }
+
+        [Fact]
+        public void TheFractionCanHaveLeadingZeros()
+        {
+            Assert.True(IsJsonNumber("0.00000001"));
+            Assert.True(IsJsonNumber("10.00000001"));
+        }
+
+        [Fact]
+        public void DoesNotEndWithADot()
+        {
+            Assert.False(IsJsonNumber("12."));
+        }
+
+        [Fact]
+        public void DoesNotHaveTwoFractionParts()
+        {
+            Assert.False(IsJsonNumber("12.34.56"));
+        }
+
+        [Fact]
+        public void TheDecimalPartDoesNotAllowLetters()
+        {
+            Assert.False(IsJsonNumber("12.3x"));
+        }
     }
 }
--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -7,7 +7,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return NumberHasContent(input) && IsInteger(ExtractInteger(input));
+            return NumberHasContent(input) && IsInteger(ExtractInteger(input)) && IsFraction(ExtractFraction(input));
         }
 
         static bool NumberHasContent(string input)
@@ -19,9 +19,9 @@ namespace Json
         {
             var indexOfExponent = input.IndexOfAny("eE".ToCharArray());
             var indexOfDot = input.IndexOf('.');
-            if (indexOfDot < indexOfExponent)
+            if (indexOfDot != indexOfExponent)
             {
-               input = input.Remove(indexOfDot, input.Length - indexOfDot);
+               input = input.Substring(0, indexOfDot);
             }
 
             foreach (char c in input)
@@ -39,6 +39,38 @@ namespace Json
         static bool IsInteger(string input)
         {
             return input.Length > 1 && input[0] != '0' || input.Length == 1;
+        }
+
+        static string ExtractFraction(string input)
+        {
+            var indexOfExponent = input.IndexOfAny("eE".ToCharArray());
+            var indexOfDot = input.IndexOf('.');
+            int numberOfDots = 0;
+            int containForbiddenLetters = 0;
+            foreach (char c in input)
+            {
+                if (c == '.')
+                {
+                    numberOfDots++;
+                }
+
+                if (char.IsLetter(c) && c != 'e' && c != 'E')
+                {
+                    containForbiddenLetters++;
+                }
+            }
+
+            if (input.Contains('.'))
+            {
+                input = indexOfDot != indexOfExponent && numberOfDots == 1 && containForbiddenLetters == 0 ? input.Substring(indexOfDot, input.Length - indexOfDot) : input.Remove(0, input.Length);
+            }
+
+            return input;
+        }
+
+        static bool IsFraction(string input)
+        {
+            return input.Length > 1 && input[^1] != '.' || input.Length == 1 && !input.Contains('.');
         }
     }
 }
