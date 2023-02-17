63dc7b3e274a7a4ab2ec29e27e269f3cb2cac6c8
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-17 16:30:59 +0200

    Add Project Classes

---
+++ ClassesProject/Range.cs
@@ -0,0 +1,25 @@
+namespace Classes
+{
+    class Range
+    {
+        private readonly char start;
+        private readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            var c = new Range('a', 'f');
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return c.start <= text[0] && text[0] <= c.end ? true : false;
+        }
+    }
+}
---
+++ ClassesProject/RangeFacts.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace Classes
+{
+    public class RangeFacts
+    {
+        [Fact]  
+        public void StringStartsWithFirstCharFromRange()
+        {
+            Range c = new Range('a', 'f');
+            Assert.True(c.Match("abc"));
+        }
+
+        [Fact]
+        public void StringStartsWithLastCharFromRange()
+        {
+            Range c = new Range('a', 'f');
+            Assert.True(c.Match("fab"));
+        }
+
+        [Fact]
+        public void StringStartsWithACharThatIsInRange()
+        {
+            Range c = new Range('a', 'f');
+            Assert.True(c.Match("bcd"));
+        }
+
+        [Fact]
+        public void StringStartsWithACharThatIsNotInRange()
+        {
+            Range c = new Range('a', 'f');
+            Assert.False(c.Match("1ab"));
+        }
+
+        [Fact]
+        public void StringIsNullOrEmpty()
+        {
+            Range c = new Range('a', 'f');
+            Assert.False(c.Match(null));
+            Assert.False(c.Match(string.Empty));
+        }
+    }
+}
