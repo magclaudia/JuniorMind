49f33f9663c32b61ad998ed58c3b6cf34f3ff19b
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-17 21:13:40 +0200

    Add Class Character

---
+++ ClassesProject/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class Character
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == pattern;
+        }
+    }
+}
---
+++ ClassesProject/CharacterFacts.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Classes
+{
+    public class CharacterFacts
+    {
+        [Fact]
+        public void StringStartsWithPattern()
+        {
+            Character c = new Character('a');
+            Assert.True(c.Match("asd1"));
+        }
+
+        [Fact]
+        public void StringDoesntStartsWithPattern()
+        {
+            Character c = new Character('1');
+            Assert.False(c.Match("dgahdg"));
+        }
+
+        [Fact]
+        public void StringIsNullOrEmpty()
+        {
+            Character c = new('a');
+            Assert.False(c.Match(null));
+            Assert.False(c.Match(string.Empty));
+        }
+    }
+}
