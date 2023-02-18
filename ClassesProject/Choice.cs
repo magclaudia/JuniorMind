99a4a8588a158a1b4685810e20b2585c3733b493
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-18 15:46:04 +0200

    Add Class Choice

---
+++ ClassesProject/Choice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Classes
+{
+    public interface IPattern
+    {
+        bool Match(string text);
+    }
+
+    public class Choice : IPattern
+    {
+        private IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Match(string text) 
+        {
+            foreach (var pattern in patterns)
+            {
+                if (pattern.Match(text))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ClassesProject/ChoiceFacts.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Classes
+{
+    public class ChoiceFacts
+    {
+        [Fact]  
+        public void Valid() 
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Range('a', 'f') , new Range('A', 'F'));
+            Assert.True(digit.Match("0"));
+            Assert.True(hex.Match("a231"));
+            Assert.True(hex.Match("B231"));
+        }
+
+        [Fact]
+        public void NotValid()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Range('a', 'f') , new Range('A', 'F'));
+            Assert.False(digit.Match("a"));
+            Assert.False(hex.Match("z"));
+        }
+
+        [Fact]
+        public void Negativ()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            Assert.True(digit.Match("-124"));
+        }
+    }
+}
