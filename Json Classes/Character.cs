f0c31af8a9acc06dbe0bee82aa4c89a3eae32277
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 16:41:32 +0200

    Update Classes Character and Range

--- Json Classes/Character.cs
+++ Json Classes/Character.cs
@@ -13,7 +13,9 @@ namespace JsonClasses
 
         public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && text[0] == pattern ? new Match(true, text[1..]) : new Match(false, text);
+            return !string.IsNullOrEmpty(text) && text[0] == pattern 
+                ? new Match(true, text[1..])
+                : new Match(false, text);
         }
     }
 }
--- Json Classes/Range.cs
+++ Json Classes/Range.cs
@@ -15,8 +15,9 @@ namespace JsonClasses
 
         public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && start <= text[0] && text[0] <= end ?
-                new Match(true, text[1..]) : new Match(false, text);
+            return !string.IsNullOrEmpty(text) && start <= text[0] && text[0] <= end 
+                ? new Match(true, text[1..])
+                : new Match(false, text);
         }
     }
 }
