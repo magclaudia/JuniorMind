23e214b4b60a8bea8bc142f39753137496bc56c8
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 23:15:51 +0200

    Add Class Optional

---
+++ Json Classes/Optional.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class Optional : IPattern
+    {
+        private readonly IPattern pattern;
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(true, text);
+            }
+
+            IMatch match = new Match(true, text);
+            if (match.Succes())
+            {
+               match = pattern.Match(match.RemainingText());
+            }
+
+            return new Match(true, match.RemainingText());
+        }
+    }
+}
---
+++ Json Classes/OptionalFacts.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void ValidString_TextContainLetterPrefix()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match("abc").Succes());
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextContainLetterPrefixWhichIsRepeatedManyTimes()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match("aabc").Succes());
+            Assert.Equal("abc", a.Match("aabc").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextDoesNotContainPrefix()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match("bc").Succes());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsEmpty()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match("").Succes());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsNull()
+        {
+            var a = new Optional(new Character('a'));
+            Assert.True(a.Match(null).Succes());
+            Assert.Null(a.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextContainSignPrefix()
+        {
+            var sign = new Optional(new Character('-'));
+            Assert.True(sign.Match("123").Succes());
+            Assert.Equal("123", sign.Match("123").RemainingText());
+            Assert.True(sign.Match("-123").Succes());
+            Assert.Equal("123", sign.Match("-123").RemainingText());
+        }
+    }
+}
