cfa37f1201c2ccef9e3ab946789ebb698e412611
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 16:38:00 +0200

    Update Any.cs

--- Json Classes/Any.cs
+++ Json Classes/Any.cs
@@ -12,8 +12,9 @@ namespace JsonClasses
 
         public IMatch Match(string text)
         {
-            return !string.IsNullOrEmpty(text) && accepted.Contains(text[0]) ?
-                new Match(true, text[1..]) : new Match(false, text);
+            return !string.IsNullOrEmpty(text) && accepted.Contains(text[0]) 
+                ? new Match(true, text[1..])
+                : new Match(false, text);
         }
     }
 }
