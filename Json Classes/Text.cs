887a891e5688e864412759b010aae1fad0cf14ec
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 21:13:55 +0200

    Add Class Text

---
+++ Json Classes/Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            if (text.StartsWith(prefix))
+            {
+                return new Match(true, text[prefix.Length..]);
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
---
+++ Json Classes/TextFacts.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class TextFacts
+    {
+        [Fact]
+        public void ValidString_StringHasRequiredPrefix() 
+        {
+            var prefix = new Text("true");
+            Assert.True(prefix.Match("true").Succes());
+            Assert.Equal("", prefix.Match("true").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_StringHasRequiredPrefixAndRemainigText()
+        {
+            var prefix = new Text("true");
+            Assert.True(prefix.Match("trueX").Succes());
+            Assert.Equal("X", prefix.Match("trueX").RemainingText());
+        }
+
+        [Fact]
+        public void InvalidString_StringHasRequiredPrefixAndRemainigText()
+        {
+            var prefix = new Text("true");
+            Assert.False(prefix.Match("false").Succes());
+            Assert.Equal("false", prefix.Match("false").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsEmptyString()
+        {
+            var prefix = new Text("true");
+            Assert.False(prefix.Match("").Succes());
+            Assert.Equal("", prefix.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsNull()
+        {
+            var prefix = new Text("true");
+            Assert.False(prefix.Match(null).Succes());
+            Assert.Null(prefix.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void PrefixAndTextIsEmpty() 
+        {
+            var empty = new Text("");
+            Assert.True(empty.Match("true").Succes());
+            Assert.Equal("true", empty.Match("true").RemainingText());
+        }
+
+        [Fact]
+        public void PrefixAndTextIsNull()
+        {
+            var empty = new Text("");
+            Assert.False(empty.Match(null).Succes());
+            Assert.Null(empty.Match(null).RemainingText());
+        }
+    }
+}
