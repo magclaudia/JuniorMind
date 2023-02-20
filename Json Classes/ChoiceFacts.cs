53c6ecfd0f2846d319dcdc219892c0689914a796
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 16:27:07 +0200

    Update ChoiceFacts.cs

--- Json Classes/ChoiceFacts.cs
+++ Json Classes/ChoiceFacts.cs
@@ -5,29 +5,50 @@ namespace JsonClasses
     public class ChoiceFacts
     {
         [Fact]  
-        public void Valid() 
+        public void ValidString_StringStartsWithRequiredChar() 
         {
             var digit = new Choice(new Character('0'), new Range('1', '9'));
             var hex = new Choice(digit, new Range('a', 'f') , new Range('A', 'F'));
             Assert.True(digit.Match("0").Succes());
-            Assert.True(hex.Match("a231").Succes());
-            Assert.True(hex.Match("B231").Succes());
+            Assert.Equal("", digit.Match("0").RemainingText());
+            Assert.True(hex.Match("a2314").Succes());
+            Assert.Equal("2314", hex.Match("a2314").RemainingText());
+            Assert.True(hex.Match("B2311").Succes());
+            Assert.Equal("2311", hex.Match("B2311").RemainingText());
         }
 
+
         [Fact]
-        public void NotValid()
+        public void StringInvalid_StringDoesNotStartWithRequiredChar()
         {
             var digit = new Choice(new Character('0'), new Range('1', '9'));
             var hex = new Choice(digit, new Range('a', 'f') , new Range('A', 'F'));
-            Assert.False(digit.Match("a").Succes());
-            Assert.False(hex.Match("z").Succes());
+            Assert.False(digit.Match("a1247").Succes());
+            Assert.Equal("a1247", digit.Match("a1247").RemainingText());
+            Assert.False(hex.Match("z1245").Succes());
+            Assert.Equal("z1245", hex.Match("z1245").RemainingText());
         }
 
         [Fact]
-        public void Negativ()
+        public void StringIsNull()
         {
             var digit = new Choice(new Character('0'), new Range('1', '9'));
-            Assert.True(digit.Match("-124").Succes());
+            var hex = new Choice(digit, new Range('a', 'f'), new Range('A', 'F'));
+            Assert.False(digit.Match(null).Succes());
+            Assert.Null(digit.Match(null).RemainingText());
+            Assert.False(hex.Match(null).Succes());
+            Assert.Null(hex.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void StringIsEmpty()
+        {
+            var digit = new Choice(new Character('0'), new Range('1', '9'));
+            var hex = new Choice(digit, new Range('a', 'f'), new Range('A', 'F'));
+            Assert.False(digit.Match("").Succes());
+            Assert.Equal("", digit.Match("").RemainingText());
+            Assert.False(hex.Match("").Succes());
+            Assert.Equal("", hex.Match("").RemainingText());
         }
     }
 }
