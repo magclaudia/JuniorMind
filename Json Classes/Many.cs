51e0c5adb1dc25ce68494693e07b1c98320daa3c
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-20 22:57:41 +0200

    Add Class Many

---
+++ Json Classes/Many.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class Many : IPattern
+    {
+        private readonly IPattern pattern;
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(true, text);
+            }
+
+            IMatch match = new Match(true, text);
+            while (match.Succes()) 
+            {
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return new Match(true, match.RemainingText());
+        }
+    }
+}
---
+++ Json Classes/ManyFacts.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void ValidString_TextContainLetterPrefixWhichIsRepeatedOnce() 
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("abc").Succes());
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextContainLetterPrefixWhichIsRepeatedManyTimes()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("aaaabc").Succes());
+            Assert.Equal("bc", a.Match("aaaabc").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextDoesNotContainPrefix()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("bc").Succes());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsEmpty()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match("").Succes());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsNull()
+        {
+            var a = new Many(new Character('a'));
+            Assert.True(a.Match(null).Succes());
+            Assert.Null(a.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextContainDigitPrefix()
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.True(digits.Match("12345ab123").Succes());
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText());
+            Assert.True(digits.Match("ab").Succes());
+            Assert.Equal("ab", digits.Match("ab").RemainingText());
+        }
+    }
+}
