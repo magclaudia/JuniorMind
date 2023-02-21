f03e9420382409abb1f42236e2c44625671346bc
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-21 17:04:44 +0200

    Add Class OneOrMore

---
+++ Json Classes/OneOrMore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(pattern, new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Classes/OneOrMoreFacts.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class OneOrMoreFacts
+    {
+        [Fact]
+        public void ValidString_TextIsEqualWithPrefix()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match("123").Succes());
+            Assert.Equal("", a.Match("123").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextContainPrefix()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match("1a").Succes());
+            Assert.Equal("a", a.Match("1a").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_TextDoesNotContainPrefix()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("bc").Succes());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsEmpty()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match("").Succes());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void StringIsNull()
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match(null).Succes());
+            Assert.Null(a.Match(null).RemainingText());
+        }
+    }
+}
