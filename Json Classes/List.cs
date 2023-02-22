ccce0c0a6baf7ec1a1133f1400ae9c89c284f0ff
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-22 12:50:37 +0200

    Add Class List

---
+++ Json Classes/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class List
+    {
+        private readonly IPattern pattern;
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Many(new Sequence(element, new Many(new Sequence(separator, element))));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Classes/ListFacts.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void ValidString_StringHasAllElementsInRequiredInterval()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("1,2,3").Succes());
+            Assert.Equal("", a.Match("1,2,3").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_StringHasAllElementsInRequiredIntervalAndRemainingCharacters()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("1,2,3,").Succes());
+            Assert.Equal(",", a.Match("1,2,3,").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_StringhasAllElementsIn()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("1a").Succes());
+            Assert.Equal("a", a.Match("1a").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_StringhasAllElements()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match("abc").Succes());
+            Assert.Equal("abc", a.Match("abc").RemainingText());
+        }
+    }
+}
