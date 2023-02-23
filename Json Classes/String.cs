c10baa86da2e15899fc125423d6cc0c7a34ef138
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-24 00:44:15 +0200

    Add Class String

---
+++ Json Classes/String.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+        public String()
+        {
+            var quote = new Character('"');
+            var hex = new Choice(new Range('1', '9'), new Range('a', 'f'), new Range('A', 'F'));
+            var escape = new Choice(new Any("\\\"/bfnrt"),
+                new Sequence(new Character('u'), hex, hex, hex, hex));
+            var character = new Choice(new Range('\u0020', '\u0021'),
+                new Range('\u0023', '\u005B'), new Range('\u005D', '\uFFFF'),
+                new Sequence(new Character('\\'), escape));
+            var characters = new Many(character);
+            pattern = new Sequence(quote, characters, quote);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Classes/StringFacts.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class StringFacts
+    {
+        [Fact]
+        public void IsWrappedInDoubleQuotes()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted("abc")).Succes());
+            Assert.Equal("", stringJson.Match(Quoted("abc")).RemainingText());
+        }
+
+        [Fact]
+        public void AlwaysStartsWithQuotes()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted("abc\"")).Succes());
+            Assert.Equal("\"", stringJson.Match(Quoted("abc\"")).RemainingText());
+        }
+
+        [Fact]
+        public void AlwaysEndsWithQuotes()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted("\"abc")).Succes());
+            Assert.Equal("abc\"", stringJson.Match(Quoted("\"abc")).RemainingText());
+        }
+
+        [Fact]
+        public void IsNotNull()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted(null)).Succes());
+            Assert.Equal("", stringJson.Match(Quoted(null)).RemainingText());
+        }
+
+
+        [Fact]
+        public void ShouldReturnEmptyStringIfStringIsEmpty()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted("")).Succes());
+            Assert.Equal("", stringJson.Match(Quoted("")).RemainingText());
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyIfStringContainsQuotationMark()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted(@"\""v\"" bp")).Succes());
+            Assert.Equal("", stringJson.Match(Quoted(@"\""v\"" bq")).RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotContainControlCharacters()
+        {
+            var stringJson = new String();
+            Assert.False(stringJson.Match(Quoted("a\nb\rc")).Succes());
+            Assert.Equal("\"a\nb\rc\"", stringJson.Match(Quoted("a\nb\rc")).RemainingText());
+        }
+
+        [Fact]
+        public void CanContainLargeUnicodeCharacters()
+        {
+            var stringJson = new String();
+            Assert.True(stringJson.Match(Quoted("⛅⚾")).Succes());
+            Assert.Equal("", stringJson.Match(Quoted("⛅⚾")).RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotEndWithAnUnfinishedHexNumber()
+        {
+            var stringJson = new String();
+            Assert.False(stringJson.Match(Quoted(@"a\u")).Succes());
+            Assert.Equal("\"a\\u\"", stringJson.Match(Quoted(@"a\u")).RemainingText());
+        }
+
+        public static string Quoted(string text)
+           => $"\"{text}\"";
+    }
+}
