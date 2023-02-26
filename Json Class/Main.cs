3a275100b8b5a5d7d6569073bc783a6d65bbfc83
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-26 22:11:15 +0200

    Add Class Main

---
+++ Json Class/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class MainClass
+    {
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            string jsonValidFormat = File.ReadAllText(args[1]);
+            var value = new Value();
+            if (value.Match(jsonValidFormat).Succes() && value.Match(jsonValidFormat).RemainingText() == "")
+            {
+                Console.WriteLine("File is a Json format");
+            }
+            else
+            {
+                Console.WriteLine("File is not a Json format");
+            }
+        }
+    }
+}
