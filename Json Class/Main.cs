cf4d91d7aba40f7a71b403efb5fc9c96bf76f29b
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-27 19:55:24 +0200

    Update Main.cs

--- Json Class/Main.cs
+++ Json Class/Main.cs
@@ -4,25 +4,25 @@ namespace JsonClasses
 {
     public class MainClass
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string[] args = Environment.GetCommandLineArgs();
-            if (args.Length == 1)
+            if (args.Length > 0)
             {
-                Console.WriteLine("Make sure you entered a valid location for json file.");
-                return;
+                string jsonFormat = File.ReadAllText(args[0]);
+                var value = new Value();
+                var match = value.Match(jsonFormat);
+                if (match.Succes() && match.RemainingText() == "")
+                {
+                    Console.WriteLine("File is a Json valid format");
+                }
+                else
+                {
+                    Console.WriteLine("File is not a Json valid format");
+                }
             }
-
-            string jsonFormat = File.ReadAllText(args[1]);
-            var value = new Value();
-            var match = value.Match(jsonFormat);
-            if (match.Succes() && match.RemainingText() == "")
+            else 
             {
-                Console.WriteLine("File is a Json valid format");
-            }
-            else
-            {
-                Console.WriteLine("File is not a Json valid format");
+                Console.WriteLine("Make sure you enter an argument.");
             }
         }
     }
