98013fb8e1aebee4588265e7e4628d20b3f8e6f8
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-02-28 11:15:21 +0200

    Update Main.cs

--- Json Class/Main.cs
+++ Json Class/Main.cs
@@ -6,6 +6,12 @@ namespace JsonClasses
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Make sure you enter an argument.");
+                return;
+            }
+
             if (args.Length > 0)
             {
                 string jsonFormat = System.IO.File.ReadAllText(args[0]);
@@ -19,10 +25,6 @@ namespace JsonClasses
                 {
                     Console.WriteLine("File is not a Json valid format");
                 }
-            }
-            else 
-            {
-                Console.WriteLine("Make sure you enter an argument.");
             }
         }
     }
