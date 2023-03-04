6c8dc3863588a6852c0d8372913f343a69015d26
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-03-04 23:23:56 +0200

    Add Project Validator Math

---
+++ Json Class/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JsonClasses
+{
+    public class Math : IPattern
+    {
+        private readonly IPattern pattern;
+        public Math()
+        {
+            var space = new Many(new Any("''"));
+            var sign = new Many(new Any("+-*/^%"));
+            var element = new Sequence(space, sign, space);
+            var digit = new Sequence(space, new OneOrMore(new Range('1', '9')), space);
+            var numbersInBrackets = new Sequence(new Character('('), digit, element, digit, new Character(')'));
+            pattern = numbersInBrackets;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Class/MathFacts.cs
@@ -0,0 +1,91 @@
+﻿using Xunit;
+
+namespace JsonClasses
+{
+    public class MathFacts
+    {
+        [Fact]
+        public void ValidString_BracketsSimpleFormula()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("( 1 + 2 )").Succes());
+            Assert.Equal("", formula.Match("( 1 + 2 )").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_BracketsComplexFormulaWithOperationOusideOfTheBrackets()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("( 1 + 2 ) * ( 3 - 4 )").Succes());
+            Assert.Equal("", formula.Match("( 1 + 2 ) * ( 3 - 4 )").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_MathComplexFormala()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ 2 + 6 % 3 * 2 ^ 3").Succes());
+            Assert.Equal("", formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ 2 + 6 % 3 * 2 ^ 3").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_ComplexFormula()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("( 2 * 5 ) - 3 ^ 2 + 9 % 3 / 3").Succes());
+            Assert.Equal("", formula.Match("( 2 * 5 ) - 3 ^ 2 + 9 % 3 / 3").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_ComplexFormulaFloatDigit()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("1.5 * 2 ^ 3 * 5 / ( 2 + 3 ) - 2 ^ 2").Succes());
+            Assert.Equal("", formula.Match("1.5 * 2 ^ 3 * 5 / ( 2 + 3 ) - 2 ^ 2").RemainingText());
+        }
+
+        [Fact]
+        public void ValidString_ComplexFormulaInBrackets()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / 6 ) * 7").Succes());
+            Assert.Equal("", formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / 6 ) * 7").RemainingText());
+        }
+
+        [Fact]
+        public void InvalidString_SignIsNotFollowedByADigit()
+        {
+            var formula = new Math();
+            Assert.False(formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ + 6 % 3 * 2 ^ 3").Succes());
+            Assert.Equal("+ 6 % 3 * 2 ^ 3", formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ + 6 % 3 * 2 ^ 3").RemainingText());
+        }
+
+        [Fact]
+        public void InvalidString_TwoSignNextToEacHOther()
+        {
+            var formula = new Math();
+            Assert.False(formula.Match("( 2 + 4 ) * / ( 2 - 1 )").Succes());
+        }
+
+        [Fact]
+        public void InvalidString_FromulaEndsWithSign()
+        {
+            var formula = new Math();
+            Assert.False(formula.Match("1.5 * 2 ^ 3 * 5 / ( 2 + 3 ) - ").Succes());
+        }
+
+        [Fact]
+        public void InvalidString_BracketsEndsWithSign()
+        {
+            var formula = new Math();
+            Assert.False(formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / ) * 7").Succes());
+        }
+
+        [Fact]
+        public void InvalidString_NoEndBracket()
+        {
+            var formula = new Math();
+            Assert.False(formula.Match("( 2 + 3").Succes());
+        }
+    }
+}
