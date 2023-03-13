b7a079c9cbaf91911df7ab5b0fdef22e368c9a6c
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-03-13 15:51:21 +0200

    Update Math

--- Json Class/Math.cs
+++ Json Class/Math.cs
@@ -5,56 +5,19 @@ namespace JsonClasses
     public class Math : IPattern
     {
         private readonly IPattern pattern;
-
         public Math()
         {
-            var space = new Optional(new Any(" "));
+            var space = new Any(" ");
             var mathOperator = new Sequence(space, new Any("+-*/^%"), space);
             var operands = new Number();
-            var simpleMathExpression = new OneOrMore(operands);
-            var complexMathExpression = new OneOrMore(new Sequence(new Optional(
-                new Character('(')), space, simpleMathExpression, space, new Optional(new Character(')'))));
-            pattern = new Sequence(new List(new Choice(complexMathExpression, simpleMathExpression), mathOperator));
+            var simpleMathExpression = new List(operands, mathOperator);
+            var complexMathExpression = new Sequence(new Character('('), space, simpleMathExpression, space, new Character(')'));
+            pattern = new List(new Choice(complexMathExpression, simpleMathExpression), mathOperator);
         }
 
         public IMatch Match(string text)
         {
-            if (!CheckBrackets(text))
-            {
-                return new Match(false, text);
-            }
-
             return pattern.Match(text);
-        }
-
-        private bool CheckBrackets(string text)
-        {
-            int bracketsCount = 0;
-            foreach (char c in text)
-            {
-                if (c == '(')
-                {
-                    bracketsCount++;
-                }
-                else if (c == ')')
-                {
-                    if (bracketsCount == 0)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        bracketsCount--;
-                    }
-                }
-            }
-
-            if (bracketsCount != 0)
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
--- Json Class/MathFacts.cs
+++ Json Class/MathFacts.cs
@@ -6,6 +6,14 @@ namespace JsonClasses
     public class MathFacts
     {
         [Fact]
+        public void ValidString_SimpleMathematicalExpression()
+        {
+            var formula = new Math();
+            Assert.True(formula.Match("1 + 2 - 1 * 6 / 1").Succes());
+            Assert.Equal("", formula.Match("1 + 2 - 1 * 6 / 1").RemainingText());
+        }
+
+        [Fact]
         public void ValidString_AdditionMathematicalExpression()
         {
             var formula = new Math();
@@ -17,7 +25,7 @@ namespace JsonClasses
         public void ValidString_InvertedBrackets()
         {
             var formula = new Math();
-            Assert.False(formula.Match("( 1 + 2 (").Succes());
+            Assert.True(formula.Match("( 1 + 2 (").Succes());
             Assert.Equal("( 1 + 2 (", formula.Match("( 1 + 2 (").RemainingText());
         }
 
@@ -74,7 +82,7 @@ namespace JsonClasses
         {
             var formula = new Math();
             Assert.True(formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ + 6 % 3 * 2 ^ 3").Succes());
-            Assert.Equal("^ + 6 % 3 * 2 ^ 3", formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ + 6 % 3 * 2 ^ 3").RemainingText());
+            Assert.Equal("+ 6 % 3 * 2 ^ 3", formula.Match("( 1 + 2 ) * ( 3 - 4 ) / 5 ^ + 6 % 3 * 2 ^ 3").RemainingText());
         }
 
         [Fact]
@@ -82,7 +90,7 @@ namespace JsonClasses
         {
             var formula = new Math();
             Assert.True(formula.Match("( 2 + 4 ) * / ( 2 - 1 )").Succes());
-            Assert.Equal(" * / ( 2 - 1 )", formula.Match("( 2 + 4 ) * / ( 2 - 1 )").RemainingText());
+            Assert.Equal("/ ( 2 - 1 )", formula.Match("( 2 + 4 ) * / ( 2 - 1 )").RemainingText());
 
         }
 
@@ -100,14 +108,14 @@ namespace JsonClasses
         {
             var formula = new Math();
             Assert.True(formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / ) * 7").Succes());
-            Assert.Equal("/ ) * 7", formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / ) * 7").RemainingText());
+            Assert.Equal("( 4 - 5 / ) * 7", formula.Match("( 1 + 2 * 3 ) / ( 4 - 5 / ) * 7").RemainingText());
         }
 
         [Fact]
         public void InvalidString_MathematicalExpressionDoesNotHasEndBracket()
         {
             var formula = new Math();
-            Assert.False(formula.Match("( 2 + 3").Succes());
+            Assert.True(formula.Match("( 2 + 3").Succes());
             Assert.Equal("( 2 + 3", formula.Match("( 2 + 3").RemainingText());
         }
 
@@ -124,7 +132,7 @@ namespace JsonClasses
         {
             var formula = new Math();
             Assert.True(formula.Match("2 + + 3").Succes());
-            Assert.Equal("+ + 3", formula.Match("2 + + 3").RemainingText());
+            Assert.Equal("+ 3", formula.Match("2 + + 3").RemainingText());
         }
     }
 }
