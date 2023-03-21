b8a1cdaf2b03779965ba195d712d3e6a3e219f02
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-03-21 13:45:05 +0200

    Update Project

---
+++ Data Collection/StringIntegers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataCollection
+{
+    class IntArray
+    {
+
+        public IntArray() 
+        {
+            //sir
+        }
+
+        public void Add(int value) 
+        {
+            // noul elem
+        }
+
+        public int Count()
+        {
+            //nr elem din sir
+        }
+    }
+}
