7222a45496a9a42b5364824853abc0a426187190
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-03-22 15:28:39 +0200

    Update Project

---
+++ Data Colletion/intArray.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace DataCollection
+{
+    class IntArray
+    {
+        int[] array;
+        public IntArray()
+        {
+            array = new int[0];
+        }
+
+        public void Add(int element) // adaugă un nou element la sfârșitul șirului 
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[^1] = element;
+        }
+
+        public int Count() // întorce numărul de elemente din șir
+        {
+            return array.Length;
+        }
+
+        public int Element(int index) // întoarce elementul de la indexul dat
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                {
+                    return array[i];
+                }
+            }
+
+            return -1;
+        }
+
+        public void SetElement(int index, int element) // modifică valoarea elementului de la indexul dat
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                {
+                    array[i] = element;
+                }
+            }
+        }
+
+        public bool Contains(int element) // întoarce true dacă elementul dat există în șir
+        {
+            return array.Contains(element);
+        }
+
+        public int IndexOf(int element) // întoarce indexul elementului sau -1 dacă elementul nu se regăsește în șir
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, int element) // adaugă un nou element pe poziția dată
+        {
+            Array.Resize(ref array, array.Length + 1);
+            for (int i = array.Length - 1; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            SetElement(index, element);
+        }
+
+        public void Clear() // șterge toate elementele din șir
+        {
+            Array.Clear(array, 0, array.Length);
+        }
+
+        public void Remove(int element) // șterge prima apariție a elementului din șir
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == element)
+                {
+                    while (i < array.Length - 1)
+                    {
+                        array[i] = array[i + 1];
+                        i++;
+                    }
+                }
+            }
+
+            Array.Resize(ref array, array.Length - 1);
+        }
+
+        public void RemoveAt(int index) // șterge elementul de pe poziția dată
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (i == index)
+                {
+                    while (i < array.Length - 1)
+                    {
+                        array[i] = array[i + 1];
+                        i++;
+                    }
+                }
+            }
+
+            Array.Resize(ref array, array.Length - 1);
+        }
+    }
+}
---
+++ Data Colletion/intArrayFacts.cs
@@ -0,0 +1,130 @@
+﻿using Xunit;
+
+namespace DataCollection
+{
+    public class IntArrayFacts
+    {
+        public class IntArrayTests
+        {
+            [Fact]
+            public void TestAddAndCount()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(3);
+                intArray.Add(5);
+                Assert.Equal(3, intArray.Count());
+            }
+
+            [Fact]
+            public void TestElement()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                Assert.Equal(1, intArray.Element(0));
+                Assert.Equal(2, intArray.Element(1));
+                Assert.Equal(3, intArray.Element(2));
+                Assert.Equal(-1, intArray.Element(3));
+            }
+
+            [Fact]
+            public void TestSetElement()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                intArray.SetElement(1, 4);
+                Assert.Equal(4, intArray.Element(1));
+            }
+
+            [Fact]
+            public void Contains()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                Assert.True(intArray.Contains(1));
+                Assert.True(intArray.Contains(2));
+                Assert.True(intArray.Contains(3));
+            }
+
+            [Fact]
+            public void TestIndexOf()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                Assert.Equal(0, intArray.IndexOf(1));
+                Assert.Equal(1, intArray.IndexOf(2));
+                Assert.Equal(2, intArray.IndexOf(3));
+                Assert.Equal(-1, intArray.IndexOf(4));
+            }
+
+            [Fact]
+            public void TestInsert()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                intArray.Insert(1, 4);
+                Assert.Equal(4, intArray.Count());
+                Assert.Equal(0, intArray.IndexOf(1));
+                Assert.Equal(1, intArray.IndexOf(4));
+                Assert.Equal(2, intArray.IndexOf(2));
+                Assert.Equal(3, intArray.IndexOf(3));
+            }
+
+            [Fact]
+            public void TestClear()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                intArray.Clear();
+                Assert.Equal(3, intArray.Count());
+                Assert.Equal(0, intArray.IndexOf(0));
+            }
+
+            [Fact]
+            public void TestRemove()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                intArray.Add(5);
+                intArray.Add(3);
+                intArray.Remove(3);
+                Assert.Equal(4, intArray.Count());
+                Assert.Equal(0, intArray.IndexOf(1));
+                Assert.Equal(1, intArray.IndexOf(2));
+                Assert.Equal(2, intArray.IndexOf(5));
+                Assert.Equal(3, intArray.IndexOf(3));
+            }
+
+            [Fact]
+            public void TestRemoveAt()
+            {
+                IntArray intArray = new IntArray();
+                intArray.Add(1);
+                intArray.Add(2);
+                intArray.Add(3);
+                intArray.Add(4);
+                intArray.Add(5);
+                intArray.RemoveAt(2);
+                Assert.Equal(4, intArray.Count());
+                Assert.Equal(0, intArray.IndexOf(1));
+                Assert.Equal(1, intArray.IndexOf(2));
+                Assert.Equal(2, intArray.IndexOf(4));
+                Assert.Equal(3, intArray.IndexOf(5));
+            }
+        }
+    }
+}
