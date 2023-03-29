4408c9ce8121608342eba431a823c74450168095
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-03-29 12:56:45 +0300

    Add Inheritance  Project Files SortedIntArray.cs and  SortedIntArrayFacts.cs

---
+++ Data Collection/SortedIntArray.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataCollection
+{
+    public class SortedIntArray
+    {
+
+    }
+}
---
+++ Data Collection/SortedIntArrayFacts.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace DataCollection
+{
+    public class SortedIntArrayFacts
+    {
+    }
+}
