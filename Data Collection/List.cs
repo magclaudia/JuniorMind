7ff12e74e9ab4d4691a87bf39eb784c9528a9be9
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-05 13:29:22 +0300

    Add new proj. List.cs and ListFacts.cs

---
+++ Data Collection/List.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataCollection
+{
+    class List
+    {
+    }
+}
---
+++ Data Collection/ListFacts.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace DataCollection
+{
+    public class ListFacts
+    {
+
+    }
+}
