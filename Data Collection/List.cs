0eec0931ab3ec4eaeeec64de8604695194b8809b
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-06 11:54:18 +0300

    Convert proj. ObjectArray.cs in List.cs

--- Data Collection/List.cs
+++ Data Collection/List.cs
@@ -1,9 +1,116 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace DataCollection
 {
-    class List
+    class List<T> : IEnumerable<T>
     {
+        private T[] array;
+        public List()
+        {
+            array = new T[4];
+        }
+
+        public int Count { get; protected set; } = 0;
+
+        public T this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        public void Add(T element)
+        {
+            Capacity();
+            array[Count++] = element;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) > -1;
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T element)
+        {
+            Capacity();
+            RightShifting(index);
+            array[index] = element;
+            Count++;
+        }
+
+        public void Remove(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    RemoveAt(i);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            Array.Clear(array);
+            Count = 0;
+        }
+
+        public void RemoveAt(int index)
+        {
+            LeftShifting(index);
+            Count--;
+        }
+
+        private void Capacity()
+        {
+            if (Count == array.Length)
+            {
+                Array.Resize(ref array, array.Length * 2);
+            }
+        }
+
+        private void RightShifting(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                array[i + 1] = array[i];
+            }
+        }
+
+        private void LeftShifting(int index)
+        {
+            for (int i = index; i <= Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+        }
+
     }
 }
--- Data Collection/ListFacts.cs
+++ Data Collection/ListFacts.cs
@@ -4,6 +4,158 @@ namespace DataCollection
 {
     public class ListFacts
     {
+        [Fact]
+        public void TestAddAndCount()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            list.Add(7);
+            list.Add(6);
+            list.Add('s');
+            list.Add(7);
+            Assert.Equal(9, list.Count);
+        }
 
+        [Fact]
+        public void TestElement()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            list.Add(7);
+            list.Add('s');
+            Assert.Equal(1, list[0]);
+            Assert.Equal("sgsdhgjd", list[1]);
+            Assert.Equal(-5, list[2]);
+            Assert.Equal(6.501, list[3]);
+            Assert.Equal('/', list[4]);
+            Assert.Equal(7, list[5]);
+            Assert.Equal('s', list[6]);
+        }
+
+        [Fact]
+        public void TestSetElement()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            list[1] = 4;
+            Assert.Equal(4, list[1]);
+        }
+
+        [Fact]
+        public void Contains()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            Assert.True(list.Contains(1));
+            Assert.True(list.Contains("sgsdhgjd"));
+            Assert.True(list.Contains(-5));
+            Assert.True(list.Contains(6.501));
+            Assert.True(list.Contains('/'));
+        }
+
+        [Fact]
+        public void TestIndexOf()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            Assert.Equal(0, list.IndexOf(1));
+            Assert.Equal(1, list.IndexOf("sgsdhgjd"));
+            Assert.Equal(2, list.IndexOf(-5));
+            Assert.Equal(3, list.IndexOf(6.501));
+            Assert.Equal(4, list.IndexOf('/'));
+            Assert.Equal(-1, list.IndexOf(4));
+        }
+
+        [Fact]
+        public void TestInsert()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            list.Insert(1, 10);
+            Assert.Equal(6, list.Count);
+            Assert.Equal(1, list[0]);
+            Assert.Equal(10, list[1]);
+            Assert.Equal("sgsdhgjd", list[2]);
+            Assert.Equal(-5, list[3]);
+            Assert.Equal(6.501, list[4]);
+            Assert.Equal('/', list[5]);
+        }
+
+        [Fact]
+        public void TestClear()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(6.501);
+            list.Add('/');
+            list.Clear();
+            Assert.Null(list[0]);
+        }
+
+        [Fact]
+        public void TestRemove()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(3);
+            list.Add(6.501);
+            list.Add('/');
+            list.Remove(3);
+            Assert.Equal(5, list.Count);
+            list.Remove(6);
+            Assert.Equal(5, list.Count);
+            Assert.Equal(1, list[0]);
+            Assert.Equal("sgsdhgjd", list[1]);
+            Assert.Equal(-5, list[2]);
+            Assert.Equal(6.501, list[3]);
+            Assert.Equal('/', list[4]);
+        }
+
+        [Fact]
+        public void TestRemoveAt()
+        {
+            var list = new List<object>();
+            list.Add(1);
+            list.Add("sgsdhgjd");
+            list.Add(-5);
+            list.Add(3);
+            list.Add(6.501);
+            list.Add('/');
+            list.RemoveAt(3);
+            Assert.Equal(5, list.Count);
+            Assert.Equal(1, list[0]);
+            Assert.Equal("sgsdhgjd", list[1]);
+            Assert.Equal(-5, list[2]);
+            Assert.Equal(6.501, list[3]);
+            Assert.Equal('/', list[4]);
+        }
     }
 }
