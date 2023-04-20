c90f4b67bec05f7ceb2ca913be070fe7f41f9a59
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-20 13:44:58 +0300

    Update ReadOnlyList.cs

--- Data Collection/ReadOnlyList.cs
+++ Data Collection/ReadOnlyList.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Data_Collection
+namespace DataCollection
 {
-    internal class ReadOnlyList
+    class ReadOnlyList<T> : IList<T>
     {
+        private readonly IList<T> originalList;
+
+        public ReadOnlyList(IList<T> originalList)
+        {
+            this.originalList = originalList;
+        }
+
+        public int Count
+        {
+            get { return originalList.Count; }
+        }
+
+        public bool IsReadOnly => originalList.IsReadOnly;
+        public T this[int index]
+        {
+            get
+            {
+                return originalList[index];
+            }
+            set
+            {
+                throw new NotSupportedException("The list is read-only.");
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            originalList.CopyTo(array, arrayIndex);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotSupportedException("The list is read-only.");
+        }
+
+        public bool Contains(T item)
+        {
+            return originalList.Contains(item);
+        }
+
+        public int IndexOf(T item)
+        {
+            return originalList.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotSupportedException("The list is read-only.");
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotSupportedException("The list is read-only.");
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException("The list is read-only.");
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotSupportedException("The list is read-only.");
+        }
     }
 }
