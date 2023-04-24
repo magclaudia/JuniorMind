e3ca1edd78773674cb05a08500744f11447704c7
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-24 15:54:08 +0300

    Update proj Stream/ Program.cs

--- Stream/Program.cs
+++ Stream/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace ReadAndWriteProject
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            WriteForFile();
+            ReadForFile();
+        }
+
+        public static void WriteForFile()
+        {
+            StreamWriter streamWriter = new StreamWriter(@"D:\Github\JuniorMind\Stream\text.txt");
+            streamWriter.WriteLine("Input Text");
+            streamWriter.Close();
+        }
+
+        public static void ReadForFile()
+        {
+            StreamReader streamReader = new StreamReader(@"D:\Github\JuniorMind\Stream\text.txt");
+            Console.WriteLine(streamReader.ReadToEnd());
+            streamReader.Close();
+        }
+    }
+}
