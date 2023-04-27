bbd2dd3e68fe074395b4394080d65caa7cf1d32e
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-27 15:36:40 +0300

    Update proj

--- Stream/Program.cs
+++ Stream/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ReadAndWriteProject
 {
@@ -7,22 +8,61 @@ namespace ReadAndWriteProject
     {
         static void Main(string[] args) 
         {
-            WriteForFile();
-            ReadForFile();
+            bool gzipped = true;
+            bool encrypted = true;
+            using MemoryStream stream = new();
+            WriteToStream(stream, gzipped, encrypted);
+            stream.Seek(0, SeekOrigin.Begin);
+            var result = ReadFromStream(stream, gzipped, encrypted);
+            Console.WriteLine(result);
         }
 
-        public static void WriteForFile()
+        public static void WriteToStream(Stream stream, bool gzipped, bool encrypted)
         {
-            StreamWriter streamWriter = new StreamWriter(@"D:\Github\JuniorMind\Stream\text.txt");
-            streamWriter.WriteLine("Input Text");
-            streamWriter.Close();
+            byte[] buffer = Encoding.UTF8.GetBytes("input text");
+            if (gzipped)
+            {
+                using var outputStream = new MemoryStream();
+                using var gzipStream = new GZipStream(outputStream, CompressionMode.Compress);
+                gzipStream.Write(buffer, 0, buffer.Length);
+                outputStream.ToArray();
+            }
+             
+            if (encrypted)
+            {
+                using var encrypt = Aes.Create();
+                encrypt.GenerateKey();
+                encrypt.GenerateIV();
+                ICryptoTransform encryptor = encrypt.CreateEncryptor(encrypt.Key, encrypt.IV);
+                using MemoryStream msEncrypt = new MemoryStream();
+                using CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+                csEncrypt.Write(buffer, 0, buffer.Length);
+                buffer = msEncrypt.ToArray();
+            }
         }
 
-        public static void ReadForFile()
+        public static string ReadFromStream(Stream stream, bool gzipped, bool encrypted)
         {
-            StreamReader streamReader = new StreamReader(@"D:\Github\JuniorMind\Stream\text.txt");
-            Console.WriteLine(streamReader.ReadToEnd());
-            streamReader.Close();
+            byte[] buffer = Encoding.UTF8.GetBytes("input text");
+
+            if (gzipped)
+            {
+                using var inputStream = new MemoryStream(buffer);
+                using var outputStream = new MemoryStream();
+                using var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress);
+                outputStream.ToArray();
+            }
+
+            if (encrypted)
+            {
+                using var aes = Aes.Create();
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using var msDecrypt = new MemoryStream(buffer);
+                using var cryptoStream = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+                using var reader = new StreamReader(cryptoStream);
+            }
+
+            return Encoding.UTF8.GetString(buffer);
         }
     }
 }
