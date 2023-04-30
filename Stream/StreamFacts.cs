e2a6d8c7867900c39be8059be8fe011d3861a7bd
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-04-30 12:44:57 +0300

    Update proj

--- Stream/StreamFacts.cs
+++ Stream/StreamFacts.cs
@@ -1,13 +1,20 @@
-﻿using Xunit;
+﻿using NUnit.Framework;
+using Xunit;
 
 namespace Stream
 {
-    public class StreamFacts
+    public class ProgramFacts
     {
         [Fact]
         public void VerifyIfRetunInputStringIfTheTextIsNOtGzipatOrEncrypt()
         {
-
+            using var stream = new MemoryStream();
+            var gzipped = false;
+            var encrypted = false;
+            new StreamReader(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            var result = new StreamReader(stream);
+            Assert.Equals("input text", result);
         }
     }
 }
