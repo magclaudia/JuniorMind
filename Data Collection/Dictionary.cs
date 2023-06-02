916f8ac3ec8cee24e4c5a30ceb6cdf8d1c862e08
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-06-02 23:34:12 +0300

    Add proj Element.cs

--- Data Collection/Dictionary.cs
+++ Data Collection/Dictionary.cs
@@ -1,27 +1,67 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DataCollection
 {
-    class Dictionary : IDictionary<string, object>
+    public class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        public object this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private Element<TKey, TValue>[] elements;
+        private int[] bucket;
+        //private int count = 0;
+        private int freeIndex = -1;
+        private int capacity;
 
-        public ICollection<string> Keys => throw new NotImplementedException();
+        public Dictionary() 
+        {
+            capacity = capacity;
+            bucket = new int[capacity];
+            elements = new Element<TKey, TValue>[capacity];
+        }
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException("Key is null.");
+                }
 
-        public ICollection<object> Values => throw new NotImplementedException();
+                if (!TryGetValue(key, out TValue value))
+                {
+                    throw new KeyNotFoundException("Key is not found.");
+                }
+
+                return value;
+            }
+            set
+            {
+                if (FindPosition(key, out var element))
+                {
+                    element.Value = value;
+                }
+                else
+                {
+                    Add(key, value);
+                }
+            }
+        }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException();
+
+        public ICollection<TValue> Values => throw new NotImplementedException();
 
         public int Count => throw new NotImplementedException();
 
         public bool IsReadOnly => throw new NotImplementedException();
 
-        public void Add(string key, object value)
+        public void Add(TKey key, TValue value)
         {
             throw new NotImplementedException();
         }
 
-        public void Add(KeyValuePair<string, object> item)
+        public void Add(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
         }
@@ -31,42 +71,47 @@ namespace DataCollection
             throw new NotImplementedException();
         }
 
-        public bool Contains(KeyValuePair<string, object> item)
+        public bool Contains(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
         }
 
-        public bool ContainsKey(string key)
+        public bool ContainsKey(TKey key)
         {
             throw new NotImplementedException();
         }
 
-        public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             throw new NotImplementedException();
         }
 
-        public bool Remove(string key)
+        public bool Remove(TKey key)
         {
             throw new NotImplementedException();
         }
 
-        public bool Remove(KeyValuePair<string, object> item)
+        public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
         }
 
-        public bool TryGetValue(string key, [MaybeNullWhen(false)] out object value)
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
         {
             throw new NotImplementedException();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool FindPosition(TKey key, out object element)
         {
             throw new NotImplementedException();
         }
---
+++ Data Collection/Element.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataCollection
+{
+    public class Element<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public int Next { get; set; } = -1;
+    }
+}
