baccd14ed9a73c59d8039d092d96f0c871469906
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-11-24 00:11:47 +0200

    Update Value class

--- Json Class/Value.cs
+++ Json Class/Value.cs
@@ -11,16 +11,13 @@ namespace JsonClasses
                 new Text("true"), new Text("false"), new Text("null"));
             var ws = new Many(new Any(" \n\r\t"));
             var element = new Sequence(ws, value, ws);
+            var elements = new List(element, new Character(','));
+            var array = new Sequence(new Character('['), ws, elements, ws, new Character(']'));
             var member = new Sequence(ws, new String(), ws, new Character(':'), ws, element);
             var members = new List(member, new Character(','));
-            var array = new Sequence(new Character('['), ws, members, ws, new Character(']'));
             var obj = new Sequence(new Character('{'), ws, members, ws, new Character('}'));
-            var arrayObjValue = new Sequence(ws, obj, ws);
-            var arrayObjList = new List(new Choice(arrayObjValue, element), new Character(','));
-            var arrayObj = new Sequence(new Character('['), ws, arrayObjList, ws, new Character(']'));
             value.Add(array);
             value.Add(obj);
-            value.Add(arrayObj);
             pattern = element;
         }
 
