71296ddc6e3b7dea3776c78d7c1226a16f6ebccc
magclaudia <123964302+magclaudia@users.noreply.github.com>
2023-12-12 15:09:47 +0200

    Add new proj/ LinqMethods.cs and LinqFacts.cs

---
+++ Linq/Linq/LinqMethods.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace Linq
+{
+    public static class LinqMethods
+    {
+        public static bool All<TSource>(this IEnumerable<TSource>? source, Func<TSource, bool>? predicate)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(predicate);
+            if (source != null && predicate != null)
+            {
+                foreach (var item in source)
+                {
+                    if (!predicate(item))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource>? source, Func<TSource, bool>? predicate)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(predicate);
+            if (source != null && predicate != null)
+            {
+                foreach (var item in source)
+                {
+                    if (predicate(item))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static TSource First<TSource>(this IEnumerable<TSource>? source, Func<TSource, bool>? predicate)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(predicate);
+            if (source != null && predicate != null)
+            {
+                foreach (var item in source)
+                {
+                    if (predicate(item))
+                    {
+                        return item;
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("No items matched the predicate or The source sequence is empty.");
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource>? source, Func<TSource, TResult>? selector)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(selector);
+            if (source != null && selector != null)
+            {
+                foreach (var item in source)
+                {
+                    yield return selector(item);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource>? source, Func<TSource, IEnumerable<TResult>>? selector)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(selector);
+            if (source != null && selector != null)
+            {
+                foreach (var item in source)
+                {
+                    foreach (var result in selector(item))
+                    {
+                        yield return result;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource>? source, Func<TSource, bool>? predicate)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(predicate);
+            if (source != null && predicate != null)
+            {
+                foreach (var item in source)
+                {
+                    if (predicate(item))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(this IEnumerable<TSource>? source, Func<TSource, TKey>? keySelector, Func<TSource, TElement>? elementSelector)
+        {
+            ThrowArgumentNullException(source);
+            ThrowArgumentNullException(keySelector);
+            ThrowArgumentNullException (elementSelector);
+            var dictionary = new Dictionary<TKey, TElement>();
+            if (source != null && keySelector != null && elementSelector != null)
+            {
+                foreach (var item in source)
+                {
+                    var key = keySelector(item);
+                    if (key == null)
+                    {
+                        ThrowArgumentNullException(key);
+
+                    }
+
+                    try
+                    {
+                        dictionary.Add(keySelector(item), elementSelector(item));
+                    }
+                    catch
+                    {
+                        throw new ArgumentException("Source contains one or more duplicate keys.");
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+
+        public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>( this IEnumerable<TFirst>? first, IEnumerable<TSecond>? second, Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            ThrowArgumentNullException(first);
+            ThrowArgumentNullException(second);
+            if (first != null && second != null)
+            {
+                using (var firstEnumerator = first.GetEnumerator())
+                {
+                    using (var secondEnumerator = second.GetEnumerator())
+                    {
+                        while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
+                        {
+                            yield return resultSelector(firstEnumerator.Current, secondEnumerator.Current);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void ThrowArgumentNullException<T>(T? item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("{0} is a null reference.", nameof(item));
+            }
+        }
+    }
+}
---
+++ Linq/LinqFacts/LinqMethodsFacts.cs
@@ -0,0 +1,199 @@
+using Linq;
+using Xunit;
+
+namespace LinqFacts
+{
+    public class LinqMethodsFacts
+    {
+
+        [Fact]
+        public void Method_All_Return_ArgumentNullException()
+        {
+            var elements = new[] { 2, -5, 31 };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.All<int>(null, element => element.Equals(20)));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.All(elements, null));
+        }
+
+        [Fact]
+        public void Method_All_Return_True_WhenAllElementsFulfillTheRequirement()
+        {
+            var elements = new[] { "sun", "summer", "sara" };
+            Assert.True(LinqMethods.All(elements, element => element.StartsWith("s")));
+        }
+
+        [Fact]
+        public void Method_All_Return_False_WhenNotAllElementsFulfillTheRequirement()
+        {
+            var elements = new[] { "sun", "summer", "Sara" };
+            Assert.False(LinqMethods.All(elements, element => element.StartsWith("s")));
+        }
+
+        [Fact]
+        public void Method_Any_Return_ArgumentNullException()
+        {
+            var elements = new[] { 2, -5, 31 };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Any<string>(null, elelemnt => elelemnt.StartsWith('s')));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Any(elements, null));
+        }
+
+        [Fact]
+        public void Method_Any_Return_True_WhenFindAnyElementFulfillTheRequirement()
+        {
+            var elements = new[] { 7, 2, 0 };
+            Assert.True(LinqMethods.Any(elements, element => element > 3));
+        }
+
+        [Fact]
+        public void Method_Any_Retun_False_WhenDontFindAnyElementThatFulfillTheRequirement()
+        {
+            var elements = new[] { 7, 0, 25 };
+            Assert.False(LinqMethods.Any(elements, element => element < 0));
+        }
+
+        [Fact]
+        public void Method_First_Return_ArgumentNullException()
+        {
+            var elements = new[] { 2, -5, 31 };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.First<int>(null, element => element > 2));
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.First(elements, null));
+        }
+
+        [Fact]
+        public void Method_First_ReturnFistElementThatFulfillTheRequirement()
+        {
+            var elements = new[] { 2, -5, 31 };
+            Assert.Equal(31, LinqMethods.First(elements, element => element > 2));
+        }
+
+        [Fact]
+        public void Method_First_ReturnInvalidOperationExceptionWhenNoItemsMatchedThePredicate()
+        {
+            var elements = new[] { 2, 5, 4 };
+            Assert.Throws<InvalidOperationException>(() => (LinqMethods.First(elements, element => element < 0)));
+        }
+
+        [Fact]
+        public void Method_Select_Return_ArgumentNullException()
+        {
+            var elements = new[] { 2, 5, 4 };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Select<int, int>(null, element => element).GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Select<int, int>(elements, null).GetEnumerator().MoveNext());
+        }
+
+        [Fact]
+        public void Method_Select_Return_TrueIfElementFulfillTheRequirement()
+        {
+            var elements = new[] { 2, -5, 3 };
+            var result = new[] { 4, -10, 6 };
+            Assert.Equal(result, LinqMethods.Select(elements, element => element * 2));
+        }
+
+        [Fact]
+        public void Method_Select_ReturnFalseIfElementDontFulfillTheRequirement()
+        {
+            var elements = new[] { 2, -5, 3 };
+            var result = new[] { 4, -10, 6 };
+            Assert.NotEqual(result, LinqMethods.Select(elements, element => element / 2));
+        }
+
+        [Fact]
+        public void Method_SelectMany_ReturnArgumentNullException()
+        {
+            var elements = new List<List<string>> { new List<string> { "dumitru", "doru" }, new List<string> { "drum", "dunare", "dumbrava" } };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.SelectMany<List<string>, string>(null, element => element).GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.SelectMany<List<string>, string>(elements, null).GetEnumerator().MoveNext());
+        }
+
+        [Fact]
+        public void Method_SelectMany_ReturnTrueIfElementFulfillTheRequirement()
+        {
+            var elements = new List<List<string>> { new List<string> { "dumitru", "doru" }, new List<string> { "drum", "dunare", "dumbrava" } };
+            var result = new List<string> { "dumitru", "doru", "drum", "dunare", "dumbrava" };
+            Assert.Equal(result, LinqMethods.SelectMany(elements, element => element));
+        }
+
+        [Fact]
+        public void Method_SelectMany_ReturnTFalseIfElementFulfillTheRequirement()
+        {
+            var elements = new List<List<string>> { new List<string> { "dumitru", "doru" }, new List<string> { "drum", "dunare", "dumbrava" } };
+            var result = new List<string> { "dumitru", "doru", "drum", "dunare" };
+            Assert.NotEqual(result, LinqMethods.SelectMany(elements, element => element));
+        }
+
+        [Fact]
+        public void Method_Where_ReturnArgumentNullException()
+        {
+            var elements = new List<int> { 2, 7, 6, -10 };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Where<int>(null, element => element.Equals(21)).GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Where<int>(elements, null).GetEnumerator().MoveNext());
+        }
+
+        [Fact]
+        public void Method_Where_ReturnTrueifElementFulfillTheRequirement()
+        {
+            var elements = new List<int> { 2, 4, -4, 13, 11, 6 };
+            var result = new List<int> { 2, 4, -4, 6 };
+            Assert.Equal(result, LinqMethods.Where(elements, element => element % 2 == 0));
+        }
+
+        [Fact]
+        public void Method_Where_Return_FalseifElementDontFulfillTheRequirement()
+        {
+            var elements = new List<int> { 2, 4, -4, 13, 11, 6 };
+            var newElements = LinqMethods.Where(elements, element => element % 2 == 0);
+            var verification = new List<int> { 2, -4, 6, 11 };
+            Assert.NotEqual(verification, newElements);
+        }
+
+
+
+        [Fact]
+        public void Method_ToDictionary_ReturnArgumentNullExcetion()
+        {
+            var elements = new List<Employee> { new Employee { SerialNumber = 10547, Name = "Pop Ioan", Occupation = "manager" },
+                 new Employee { SerialNumber = 98974, Name = "Muresan Maria", Occupation = "engineering"},
+                 new Employee { SerialNumber = 4648798, Name = "Doru Ana", Occupation = "teacher" } };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.ToDictionary<int, string, string>(null, element => element.ToString(), element => element.ToString()));
+        }
+
+        [Fact]
+        public void Method_ToDictionary_ReturnTrueIfElementFulfillTheRequirement()
+        {
+            var elements = new List<Employee>{ new Employee { SerialNumber = 10547, Name = "Pop Ioan", Occupation = "manager" },
+                new Employee { SerialNumber = 98974, Name = "Muresan Maria", Occupation = "engineering" },
+                new Employee { SerialNumber = 4648798, Name = "Doru Ana", Occupation = "teacher" } };
+
+            var dictionary = LinqMethods.ToDictionary(elements, element => element.SerialNumber, element => element);
+            foreach (var element in elements)
+            {
+                Assert.True(dictionary.ContainsKey(element.SerialNumber));
+                Assert.Equal(element, dictionary[element.SerialNumber]);
+            }
+        }
+
+        [Fact]
+        public void Method_Zip_ReturnArgumentNullExcetion()
+        {
+            var first = new List<int> { 1, 2, 3, 4 };
+            var second = new List<string> { "unu", "doi", "trei", "patru" };
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Zip<int, string, string>(null, second, (firstSeq, secondSeq) => firstSeq + " " + secondSeq).GetEnumerator().MoveNext());
+            Assert.Throws<ArgumentNullException>(() => LinqMethods.Zip<int, string, string>(first, null, (firstSeq, secondSeq) => firstSeq + " " + secondSeq).GetEnumerator().MoveNext());
+        }
+
+        [Fact]
+        public void Method_Zip_ReturnTrueIfElementFulfillTheRequirement()
+        {
+            var first = new List<int> { 1, 2, 3, 4 };
+            var second = new List<string> { "unu", "doi", "trei", "patru" };
+            var resultExpected = new List<string> { "1 unu", "2 doi", "3 trei", "4 patru" };
+            Assert.Equal(resultExpected, LinqMethods.Zip(first, second, (firstSeq, secondSeq) => firstSeq + " " + secondSeq));
+        }
+    }
+
+    public class Employee
+    {
+        public int SerialNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Occupation { get; set; }
+    }
+}
