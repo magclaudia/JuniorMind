424c9794d5164950eb0fbe33aeda57075c6b38ce
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-01-17 11:36:50 +0200

    Add class Product and method Add

---
+++ Linq/Linq/Stock/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqStock
+{
+    public class Product
+    {
+        public Product(string name, int quantity)
+        {
+            this.ProductName = name;
+            this.Quantity = quantity;
+        }
+
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Linq/Linq/Stock/Stock.cs
+++ Linq/Linq/Stock/Stock.cs
@@ -7,9 +7,21 @@ namespace LinqStock
 {
     public class Stock
     {
+        private readonly List<Product> productList;
         public Stock() 
         {
-
+            productList = new List<Product>();
         }
+
+        public void Add(Product product)
+        {
+            if (productList.Contains(product))
+            {
+                throw new ArgumentException("This product exist in stock.");
+            }
+
+            productList.Add(product);
+        }
+
     }
 }
