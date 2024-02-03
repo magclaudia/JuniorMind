ea75a64a9f42f6523cfeb2fad4938684469bd9fe
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-02-04 00:40:26 +0200

    First try without using linq extensions methods

--- Linq/Linq/LinqMethodsOnStrings/LinqMethodsOnStrings.cs
+++ Linq/Linq/LinqMethodsOnStrings/LinqMethodsOnStrings.cs
@@ -7,6 +7,24 @@ namespace LinqLinqMethodsOnStrings
 {
     public class LinqMethodsOnStrings
     {
+        public static (int, int) VowelsAndConsonants(string text)
+        {
+            char[] isVowels = { 'a', 'e', 'i', 'o', 'u'};
+            int countVowels = 0;
+            int countConsonants = 0;
+            foreach (char c in text) 
+            {
+                if (char.IsLetter(c) && isVowels.Contains(c))
+                {
+                    countVowels++;
+                }
+                else if (char.IsLetter(c))
+                {
+                    countConsonants++;
+                }
+            }
 
+            return (countConsonants, countVowels);
+        }
     }
 }
--- Linq/LinqFacts/LinqMethodsOnStringsFacts/LinqMethodsOnStringsFacts.cs
+++ Linq/LinqFacts/LinqMethodsOnStringsFacts/LinqMethodsOnStringsFacts.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace LinqLinqMethodsOnStrings
 {
     public class LinqMethodsOnStringsFacts
     {
+        [Fact]
+        public void Identify_Vowels_And_Consonants()
+        {
+            string text = "zaeiout1";
+            (int, int) result = LinqMethodsOnStrings.VowelsAndConsonants(text);
+            (int, int) expectedResult = (2, 5); 
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
