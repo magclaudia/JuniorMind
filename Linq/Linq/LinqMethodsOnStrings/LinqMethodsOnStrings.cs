b11232ba35c738a1fb7789d589b188a09ea0d586
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-02-08 13:56:21 +0200

    Update LinqMethodsOnStrings.cs

--- Linq/Linq/LinqMethodsOnStrings/LinqMethodsOnStrings.cs
+++ Linq/Linq/LinqMethodsOnStrings/LinqMethodsOnStrings.cs
@@ -10,10 +10,8 @@ namespace LinqLinqMethodsOnStrings
         public static (int consonants, int vowels) VowelsAndConsonants(string text)
         {
             string isVowels = "aeiouAEIOU";
-            int vowels = 0;
-            int consonants = 0;
-            return text.Aggregate((consonants, vowels), (count, c) => char.IsLetter(c) ? 
-            (isVowels.Contains(c) ? (count.consonants, count.vowels + 1) : (count.consonants + 1, count.vowels)) : count);
+            return  text.Where(char.IsLetter).Aggregate((consonants: 0, vowels: 0), (count, c) =>
+            isVowels.Contains(c) ? (count.consonants, count.vowels + 1) : (count.consonants + 1, count.vowels));
         }
     }
 }
