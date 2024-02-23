b0513d09e736c1def04dca7579f282afa9dcf6db
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-02-23 16:39:14 +0200

    Add method CompareSublistIntegersSum and test

--- Linq/Linq/LinqMethodsOnIntegers/LinqMethodsOnIntegers.cs
+++ Linq/Linq/LinqMethodsOnIntegers/LinqMethodsOnIntegers.cs
@@ -7,6 +7,20 @@ namespace LinqMethodsOnIntegers
 {
     public class LinqMethodsOnIntegers
     {
+        public static IEnumerable<IEnumerable<int>> CompareSublistIntegersSum(int[] integers, int k)
+        {
+            return GetSublists(integers).Where(sublist => sublist.Sum() <= k);
+        }
 
+        private static IEnumerable<IEnumerable<int>> GetSublists(int[] integers)
+        {
+            for (int i = 0; i < integers.Length; i++)
+            {
+                for (int j = i + 1; j <= integers.Length; j++)
+                {
+                    yield return integers[i..j];
+                }
+            }
+        }
     }
 }
--- Linq/LinqFacts/LinqMethodsOnIntegersFacts/LinqMethodsOnIntegersFacts.cs
+++ Linq/LinqFacts/LinqMethodsOnIntegersFacts/LinqMethodsOnIntegersFacts.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
-namespace LinqMethodsOnIntegersFacts
+namespace LinqMethodsOnIntegers
 {
     public class LinqMethodsOnIntegersFacts
     {
+        [Fact]
+        public void CompareSubstringsSum()
+        {
+            int[] integers = { 1, 2, 3, 4, 5 };
+            int k = 8;
+            var result = LinqMethodsOnIntegers.CompareSublistIntegersSum(integers, k);
+            var expected = new List<List<int>> 
+            { 
+                new List<int> { 1 }, 
+                new List<int> { 1, 2 },
+                new List<int> { 1, 2, 3 },
+                new List<int> { 2 },
+                new List<int> { 2, 3 },
+                new List<int> { 3 },
+                new List<int> { 3, 4 },
+                new List<int> { 4 },
+                new List<int> { 5 } 
+            };
+
+            Assert.Equal(expected, result);
+        }
     }
 }
