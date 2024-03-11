775475d40744fef62115ecb1bf8d8be81520a646
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-03-11 22:05:49 +0200

    Add new classes Product, Feature and ProductFeatures

---
+++ Linq/Linq/Product/ProductFeatures.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProduct 
+{
+    public class ProductFeatures
+    {
+        public static IEnumerable<Product> AtLeastOneFeature(List<Product> productsList, List<Feature> featuresList)
+        {
+            var list = new List<Product>();
+            foreach (Product product in productsList) 
+            {
+                foreach (Feature feature in featuresList)
+                {
+                    if (product.Features.Any(element => element.Id == feature.Id))
+                    {
+                        list.Add(product);
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Linq/LinqFacts/ProductFacts/ProductFeaturesFacts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LinqProduct
+{
+    public class ProductFeaturesFacts
+    {
+        [Fact]
+        public void Product_ProductsThatHasAtLeastOneProduct()
+        {
+            var oneCode = new Feature { Id = 1102 };
+            var treeCode = new Feature { Id = 3648 };
+            var fourCode = new Feature { Id = 401 };
+            var fiveCode = new Feature { Id = 5987 };
+            var sixCode = new Feature { Id = 6634 };
+            var nineCode = new Feature { Id = 9124 };
+            var sixeCode = new Feature { Id = 6801 };
+            
+            var features = new List<Feature>()
+            {
+                treeCode, fourCode, fiveCode, sixCode
+            };
+
+            var minge = new Product { Name = "minge", Features = new List<Feature>() { oneCode, treeCode } };
+            var papusa = new Product { Name = "papusa", Features = new List<Feature>() { treeCode } };
+            var bile = new Product { Name = "bile", Features = new List<Feature>() { sixeCode, oneCode} };
+            var urs = new Product { Name = "urs", Features = new List<Feature>() { fiveCode, nineCode } };
+            
+            var products = new List<Product>()
+            {
+                minge, papusa, bile, urs
+            };
+
+            var result = ProductFeatures.AtLeastOneFeature(products, features);
+            var expectedResult = new List<Product>()
+            {
+                minge, papusa, urs
+            };
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
