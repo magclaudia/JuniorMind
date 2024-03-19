57c36dfa47dc96fbeb2406d9f4db0b94eeeb8bd1
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-03-19 19:21:15 +0200

    Create TestResultFacts.cs

---
+++ Linq/LinqFacts/TestResultFacts/TestResultFacts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestResultsLinq
+{
+    public class TestResultsFacts
+    {
+        [Fact]
+        public void FamilyResult()
+        {
+            var list = new List<TestResults>
+            {
+                new TestResults { Id = "202", FamilyId = "500", Score = 30 },
+                new TestResults { Id = "102", FamilyId = "200", Score = 10 },
+                new TestResults { Id = "202", FamilyId = "500", Score = 30 },
+                new TestResults { Id = "32", FamilyId = "62", Score = 101 },
+                new TestResults { Id = "230", FamilyId = "200", Score = 455 },
+                new TestResults { Id = "10", FamilyId = "500", Score = 101 }
+            };
+
+            var result = TestResults.HighestScore(list);
+            var expected = new List<TestResults>
+            {
+                new TestResults { Id = "10", FamilyId = "500", Score = 101 },
+                new TestResults { Id = "230", FamilyId = "200", Score = 455 },
+                new TestResults { Id = "32", FamilyId = "62", Score = 101 }
+            };
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
