daf1149f67bffff66b6e7d6d8d4d2e6d650fdbb4
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-03-19 19:22:45 +0200

    Create TestResult.cs

---
+++ Linq/Linq/TestResult/TestResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestResultsLinq
+{
+    public class TestResults
+    {
+        public string Id { get; set; }
+        public string FamilyId { get; set; }
+        public int Score { get; set; }
+
+        public static IEnumerable<TestResults> HighestScore(IEnumerable<TestResults> list)
+        {
+
+        }
+    }
+}
