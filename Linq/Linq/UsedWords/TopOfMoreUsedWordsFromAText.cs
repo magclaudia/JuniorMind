ec448d69a0c96e47e0a1155232c550f3b889f9a5
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-03-20 12:53:59 +0200

    add method MoreUsedWords and test

---
+++ Linq/Linq/UsedWords/TopOfMoreUsedWordsFromAText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsedWords
+{
+    public class TopOfMoreUsedWordsFromAText
+    {
+        public static IEnumerable<string> MoreUsedWords(string text)
+        {
+
+        }
+    }
+}
---
+++ Linq/LinqFacts/UsedWordsFacts/TopOfMoreUsedWordsFromATextFacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UsedWords
+{
+    public class TopOfMoreUsedWordsFromATextFacts
+    {
+        [Fact]
+        public void MoreUsedWords()
+        {
+            var text = "Ana are mere verzi pe care i le vinde lui Ovidiu care are mere galbene pe care i le vinde Anei pentru ca ea nu are .";
+            var result = TopOfMoreUsedWordsFromAText.MoreUsedWords(text);
+            var expected = new List<string> { "are", "care", "mere", "i", "le", "vinde", "Ana", "verzi", "lui", 
+                "Ovidiu", "galbene", "Anei", "pentru", "ca", "ea", "nu" };
+            Assert.Equal(expected, result);
+        }
+    }
+}
