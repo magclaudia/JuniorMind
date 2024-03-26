24950fb43ff41ae2275f4cc4ccc41cb61a4108ac
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-03-26 15:23:49 +0200

    add Sudoku class and tests/ use to check if a input sudoku table is a valid sudoku

---
+++ Linq/Linq/Sudoku/Sudoku.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLinq
+{
+    public class Sudoku
+    {
+        public static bool IsValidSudoku(int[,] sudokuBoard)
+        {
+           return Enumerable.Range(0, 9).All(j => Line(sudokuBoard, j) && Column(sudokuBoard, j) && Block(sudokuBoard, j));
+        }
+
+        public static bool Line(int[,] sudokuBoard, int j)
+        {
+            return Enumerable.Range(0, 9).Select(i => sudokuBoard[j, i]).GroupBy(key => key).All(group => group.Count() == 1);
+        }
+           
+        private static bool Column(int[,] sudokuBoard, int j)
+        {
+            return Enumerable.Range(0, 9).Select(i => sudokuBoard[i, j]).GroupBy(key => key).All(group => group.Count() == 1);
+        }
+
+        private static bool Block(int[,] sudokuBoard, int position)
+        {
+            var line = Enumerable.Range(position / 3 * 3, 3);
+            var column = Enumerable.Range(position % 3 * 3, 3);
+            var block = line.SelectMany(j => column, (i, j) => sudokuBoard[i, j]);
+            return block.GroupBy(key => key).All(group => group.Count() == 1);
+        }
+    }
+}
---
+++ Linq/LinqFacts/SudokuFacts/SudokuFacts.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SudokuLinq
+{
+    public class SudokuFacts
+    {
+        [Fact]
+        public void IsValidSudoku()
+        {
+            var sudokuBoard = new int [,] {  { 9, 1, 8,  5, 7, 2,  6, 4, 3 },
+                                             { 7, 5, 3,  6, 9, 4,  1, 8, 2 },
+                                             { 2, 6, 4,  1, 8, 3,  7, 9, 5 },
+                                            
+                                             { 1, 9, 6,  4, 2, 8,  5, 3, 7 },
+                                             { 3, 8, 2,  7, 5, 6,  9, 1, 4 },
+                                             { 5, 4, 7,  9, 3, 1,  8, 2, 6 },
+                                            
+                                             { 4, 7, 9,  2, 1, 5,  3, 6, 8 },
+                                             { 8, 2, 5,  3, 6, 9,  4, 7, 1 },
+                                             { 6, 3, 1,  8, 4, 7,  2, 5, 9 } 
+                                          };
+            
+            var result = Sudoku.IsValidSudoku(sudokuBoard);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void InvalidLine()
+        {
+            var sudokuBoard = new int[,] {   { 9, 1, 8,  5, 7, 2,  6, 4, 3 },
+                                             { 7, 5, 3,  6, 9, 4,  1, 8, 7 },
+                                             { 2, 6, 4,  1, 8, 3,  7, 9, 5 },
+
+                                             { 1, 9, 6,  4, 2, 8,  5, 3, 7 },
+                                             { 3, 8, 2,  7, 5, 6,  9, 1, 4 },
+                                             { 5, 4, 7,  9, 3, 1,  8, 2, 6 },
+
+                                             { 4, 7, 9,  2, 1, 5,  3, 6, 8 },
+                                             { 8, 2, 5,  3, 6, 9,  4, 7, 1 },
+                                             { 6, 3, 1,  8, 4, 7,  2, 5, 9 }
+                                          };
+
+            var result = Sudoku.IsValidSudoku(sudokuBoard);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void InvalidColumn()
+        {
+            var sudokuBoard = new int[,] {   { 9, 1, 8,  5, 7, 2,  6, 4, 3 },
+                                             { 7, 5, 3,  6, 9, 4,  1, 8, 2 },
+                                             { 2, 6, 4,  1, 8, 3,  7, 9, 5 },
+
+                                             { 1, 9, 6,  4, 2, 8,  5, 3, 7 },
+                                             { 3, 8, 2,  7, 5, 6,  9, 1, 4 },
+                                             { 5, 4, 7,  9, 3, 1,  8, 2, 6 },
+
+                                             { 4, 7, 9,  2, 1, 5,  3, 6, 8 },
+                                             { 8, 2, 5,  3, 6, 9,  4, 7, 1 },
+                                             { 6, 1, 1,  8, 4, 7,  2, 5, 9 }
+                                          };
+
+            var result = Sudoku.IsValidSudoku(sudokuBoard);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void InvalidBlock()
+        {
+            var sudokuBoard = new int[,] {   { 9, 1, 8,  5, 7, 2,  6, 4, 3 },
+                                             { 7, 5, 3,  6, 9, 4,  1, 8, 2 },
+                                             { 2, 6, 4,  1, 8, 3,  7, 9, 5 },
+
+                                             { 1, 9, 6,  4, 2, 8,  5, 3, 7 },
+                                             { 3, 8, 2,  7, 5, 6,  9, 1, 4 },
+                                             { 5, 4, 7,  9, 3, 1,  8, 2, 6 },
+
+                                             { 1, 7, 9,  2, 1, 5,  3, 6, 8 },
+                                             { 8, 2, 5,  3, 6, 9,  4, 7, 1 },
+                                             { 6, 3, 1,  8, 4, 7,  2, 5, 9 }
+                                          };
+
+            var result = Sudoku.IsValidSudoku(sudokuBoard);
+            Assert.False(result);
+        }
+    }
+}
