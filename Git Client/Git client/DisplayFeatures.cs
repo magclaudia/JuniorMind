3ec3f6a6adb9a375a6f6c272cdf8667d8c2ad342
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-04-04 12:09:14 +0300

    add project Git Client with methods DisplayTotalNumberOfCommits and AccessRepository

---
+++ Git Client/Git client/DisplayFeatures.cs
@@ -0,0 +1,65 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClient
+{
+    public class DisplayFeatures
+    {
+        public static void DisplayTotalNumberOfCommits()
+        {
+            var command = Console.ReadLine();
+            while (command != "e")
+            {
+                if (command == "1")
+                {
+                    AccessRepository();
+                    break;
+                }
+
+                Console.WriteLine("You enter a invalid command please try again.");
+                command = Console.ReadLine();
+                if (command == "e")
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void AccessRepository()
+        {
+            Console.WriteLine("\nPlease add your repository path here, and then press \"ENTER\":");
+            var repositoryPath = Console.ReadLine();
+
+            bool repoFound = false;
+            while (repoFound == false)
+            {
+                if (repositoryPath == "e")
+                {
+                    break;
+                }
+                
+                try
+                {
+                    var repo = new Repository(repositoryPath);
+                    repoFound = true;
+                    int commitsCount = 0;
+                    foreach (var commit in repo.Commits)
+                    {
+                        commitsCount++;
+                    }
+
+                    Console.WriteLine($"Total number of commits from the given repository is: {commitsCount}");
+                }
+                catch
+                {
+                    Console.WriteLine("The repository was not found. Please input a new path.");
+                    repositoryPath = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Git Client/Git client/Main.cs
@@ -0,0 +1,13 @@
+﻿namespace GitClient
+{
+    public class GitClient
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, this is a Git client custom application.\n");
+            Console.WriteLine("Choose between the following actions: \n '1' - for displaying total number of commits from your  repository; \n 'e' - if you want to EXIT; \n after that press 'ENTER' to continue.\n");
+            Console.WriteLine("Selected command:");
+            DisplayFeatures.DisplayTotalNumberOfCommits();
+        }
+    }
+}
