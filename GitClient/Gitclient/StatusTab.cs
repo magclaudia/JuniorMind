75950d802780df49ec3e7bc42f39e84e956ec657
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-10-13 00:00:16 +0300

    Update StatusTab.cs

--- GitClient/Gitclient/StatusTab.cs
+++ GitClient/Gitclient/StatusTab.cs
@@ -32,7 +32,7 @@ namespace GitClient
 
                 if (numDeltas == 0)
                 {
-                    Console.SetCursorPosition(1, 3);
+                    Console.SetCursorPosition(1, 4);
                     string text = Tabs.SetTabTextLength("No changes found in the unstaging area.", Console.WindowWidth / 2 - 3);
                     Console.WriteLine(text);
                 }
