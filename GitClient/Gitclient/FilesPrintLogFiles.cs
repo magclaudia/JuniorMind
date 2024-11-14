9a9858ed3f6ec736cf2479b61fb5c1ec9802336a
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-11-14 16:32:37 +0200

    Update FilesPrintLogFiles.cs

--- GitClient/Gitclient/FilesPrintLogFiles.cs
+++ GitClient/Gitclient/FilesPrintLogFiles.cs
@@ -10,36 +10,16 @@ namespace GitClient
     {
         public static void PrintFilesForLog(GetCertainList list, GetVariablesForCommits variablesForCommits, GetVariablesForFiles variablesForFiles)
         {
-            var position = new DrawPanelRigthSide.FilesBox();
-            string directoryName = Path.GetDirectoryName(variablesForFiles.filePath)!;
-            variablesForFiles.projName = $"  ▾{directoryName}";
-
-            Console.ForegroundColor = ConsoleColor.White;
-            if (variablesForCommits.right == true)
-            {
-                Console.SetCursorPosition(1, position.edgeOneY);
-                Console.Write($"Files: {list.listOfFiles.Count} ");
-                Console.SetCursorPosition(1, position.edgeOneY + 1);
-                Console.WriteLine(variablesForFiles.projName);
-            }
-            else
-            {
-                Console.SetCursorPosition(position.edgeOneX + 1, position.edgeOneY);
-                Console.Write($"Files: {list.listOfFiles.Count} ");
-                Console.SetCursorPosition(position.edgeOneX + 1, position.edgeOneY + 1);
-                Console.WriteLine(variablesForFiles.projName);
-            }
-
-            Console.ResetColor();
-
+            TextToBeResize(list, variablesForCommits, variablesForFiles);
+            DrawPanelRigthSide.FilesBox filesBox = new DrawPanelRigthSide.FilesBox();
             int y = 0;
             int x = 0;
-            int height = Console.WindowHeight - variablesForFiles.fileRow - 1;
+            int height = filesBox.edgeFourY - filesBox.edgeOneY - 2;
 
-            if (height > list.listOfFiles.Count)
-            {
-                height = list.listOfFiles.Count;
-            }
+            //if (height > list.listOfFiles.Count)
+            //{
+            //    height = list.listOfFiles.Count;
+            //}
 
             if (variablesForCommits.enter == true && variablesForCommits.pressRight < 1)
             {
@@ -52,11 +32,95 @@ namespace GitClient
                 x = 1;
             }
 
-            for (int i = 0; i < height; i++)
+            int count = 0;
+            int i = 0;
+
+            if (variablesForCommits.pressRight == 1 && list.listOfFiles.Count > height)
             {
+                i = list.logFilesStartAt[variablesForFiles.fileLogStartAt];
+                //variablesForFiles.fileIndex = i;
+            }
+
+            while (count < height)
+            {
+                if (i == list.listOfFiles.Count)
+                {
+                    break;
+                }
+
                 GetFiles.ChooseColorForEachFiles(variablesForFiles, variablesForCommits, list, x, y, i, list.listOfFiles);
                 y++;
+                i++;
+                count++;
             }
+        }
+
+        private static void TextToBeResize(GetCertainList list, GetVariablesForCommits variablesForCommits, GetVariablesForFiles variablesForFiles)
+        {
+            DrawPanelRigthSide.FilesBox position = new DrawPanelRigthSide.FilesBox();
+            DrawTabs.Dimensions dimensions = new DrawTabs.Dimensions();
+            string directoryName = Path.GetDirectoryName(variablesForFiles.filePath)!;
+            variablesForFiles.projName = $"  ▾{directoryName}";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (variablesForCommits.right == true)
+            {
+                Console.SetCursorPosition(1, position.edgeOneY);
+                Console.Write($"Files: {list.listOfFiles.Count} ");
+                Console.SetCursorPosition(1, position.edgeOneY + 1);
+                Console.WriteLine(HandleResizingText(variablesForFiles.projName, dimensions, variablesForCommits, variablesForFiles));
+            }
+            else
+            {
+                Console.SetCursorPosition(position.edgeOneX + 1, position.edgeOneY);
+                Console.Write($"Files: {list.listOfFiles.Count} ");
+                Console.SetCursorPosition(position.edgeOneX + 1, position.edgeOneY + 1);
+                Console.WriteLine(HandleResizingText(variablesForFiles.projName, dimensions, variablesForCommits, variablesForFiles));
+            }
+
+            Console.ResetColor();
+            List<string> filesLog = new List<string>();
+
+            foreach (var file in list.listOfFiles)
+            {
+                filesLog.Add(HandleResizingText(file, dimensions, variablesForCommits, variablesForFiles));
+            }
+
+            list.listOfFiles.Clear();
+            list.listOfFiles = filesLog;
+            
+            if (variablesForCommits.pressRight == 1)
+            {
+                int height = position.edgeFourY - position.edgeOneY - 2;
+                int count = height;
+                list.logFilesStartAt.Add(0);
+
+                while (count < list.listOfFiles.Count)
+                {
+                    list.logFilesStartAt.Add(count);
+                    count += height;
+                }
+            }
+        }
+
+        private static string HandleResizingText(string text, DrawTabs.Dimensions dimensions, GetVariablesForCommits variablesForCommits, GetVariablesForFiles variablesForFiles)
+        {
+            if (variablesForCommits.right == true)
+            {
+                if (text.Length > dimensions.widthRightLog)
+                {
+                    text = text.Substring(0, dimensions.widthRightLog);
+                }
+            }
+            else
+            {
+                if (text.Length > dimensions.widthEnterLog)
+                {
+                    text = text.Substring(0, dimensions.widthEnterLog);
+                }
+            }
+
+            return text;
         }
     }
 }
