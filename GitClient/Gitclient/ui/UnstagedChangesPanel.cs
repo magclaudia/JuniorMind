60f8b35028dddffc50d0620648236b2e87f1cfa7
magclaudia <123964302+magclaudia@users.noreply.github.com>
2024-12-17 01:03:57 +0200

    Update UnstagedChangesPanel.cs

--- GitClient/Gitclient/ui/UnstagedChangesPanel.cs
+++ GitClient/Gitclient/ui/UnstagedChangesPanel.cs
@@ -88,7 +88,7 @@ namespace GitClient.ui
             }
             else
             {
-                Console.SetCursorPosition(6, dimensions.changesPanelHeight / 2 + dimensions.tabHeight);
+                Console.SetCursorPosition(2, dimensions.changesPanelHeight / 2 + dimensions.tabHeight);
                 string text = TextSettings.GetTextLength("No changes found in the unstaged area", dimensions.changesPanelWidth - 4);
                 Console.Write(text);
             }
